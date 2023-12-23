e35a9b704c5f90cfe81caa6dff7e801125e924dc
DeadVafel <0202052003@mail.ru>
2023-12-24 01:06:48 +0500

    Initial commit

---
+++ WebAPI_base-main/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import models
+
+from database import engine
+from routes import router_websocket, router_categories, router_items
+
+models.Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory="templates")
+
+app = FastAPI(
+    title="WebSocketChatCRUDNotify",
+    summary="WebSocket Chat + Notifications of CRUD operations!",
+    version="0.0.1",
+)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    server_urn = f"{request.scope.get('server')[0]}:{request.scope.get('server')[1]}"
+    return templates.TemplateResponse("index.html", {"request": request, "server_urn": server_urn})
+
+
+app.include_router(router_websocket)
+app.include_router(router_categories)
+app.include_router(router_items)
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run('main:app', host='192.168.1.42', port=3000, reload=True)
---
+++ WebAPI_base-main/schemas.py
@@ -0,0 +1,46 @@
+from typing import Optional
+
+from pydantic import BaseModel, ConfigDict
+from datetime import datetime
+
+
+class CategoryBase(BaseModel):
+    name: str
+
+
+class CategoryCreate(CategoryBase):
+    pass
+
+
+class CategoryUpdate(CategoryBase):
+    name: Optional[str] = None
+
+
+class Category(CategoryBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+
+class ItemBase(BaseModel):
+    name: str
+    category_id: int
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class ItemUpdate(ItemBase):
+    name: Optional[str] = None
+    category_id: Optional[int] = None
+
+
+class Item(ItemBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    created_at: datetime
+    updated_at: datetime
